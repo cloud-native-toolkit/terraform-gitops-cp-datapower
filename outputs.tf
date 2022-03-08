04276bb304850176b4d1402e4b74bf8bd57a5df1
kanand3 <keshav.anand@outlook.com>
2022-03-08 11:17:12 +0530

    Fixes to outputs.tf
    Signed-off-by: Cloud-Native Toolkit <cloudnativetoolkit@gmail.com>

--- outputs.tf
+++ outputs.tf
@@ -1,36 +1,15 @@
 
-output "name" {
-  description = "The name of the module"
-  value       = local.name
-  depends_on  = [null_resource.setup_gitops]
+output "dpInstanceName" {
+  description = "The name of DataPower instance"
+  value       = var.dpInstanceName
 }
 
-output "branch" {
-  description = "The branch where the module config has been placed"
-  value       = local.application_branch
-  depends_on  = [null_resource.setup_gitops]
+output "dpWebuiConfigMap" {
+  description = "Name of configmap which contains the configuration to enable WebUI"
+  value       = var.dpWebuiConfigMap
 }
 
-output "namespace" {
-  description = "The namespace where the module will be deployed"
-  value       = local.namespace
-  depends_on  = [null_resource.setup_gitops]
-}
-
-output "server_name" {
-  description = "The server where the module will be deployed"
-  value       = var.server_name
-  depends_on  = [null_resource.setup_gitops]
-}
-
-output "layer" {
-  description = "The layer where the module is deployed"
-  value       = local.layer
-  depends_on  = [null_resource.setup_gitops]
-}
-
-output "type" {
-  description = "The type of module where the module is deployed"
-  value       = local.type
-  depends_on  = [null_resource.setup_gitops]
+output "adminPasswordSecret" {
+  description = "Secret which stores admin password"
+  value = var.passwordSecret
 }
