c2d3fc61185348b201fe8136cbb32a815ae6caab
kanand3 <keshav.anand@outlook.com>
2022-03-13 18:28:26 +0530

    Removing unsupported variables from stage1-datapower-operator.tf
    Signed-off-by: Cloud-Native Toolkit <cloudnativetoolkit@gmail.com>

--- test/stages/stage1-datapower-operator.tf
+++ test/stages/stage1-datapower-operator.tf
@@ -5,7 +5,5 @@ module "datapower-operator" {
   git_credentials = module.gitops.git_credentials
   server_name = module.gitops.server_name
   namespace = module.gitops_namespace.name
-  catalog = module.cp_catalogs.catalog_ibmoperators
   kubeseal_cert = module.gitops.sealed_secrets_cert
-  entitlement_key = module.cp_catalogs.entitlement_key
 }
