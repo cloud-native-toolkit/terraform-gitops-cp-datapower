c5842a74db7720d08b68a98b887afb46b5423e47
kanand3 <keshav.anand@outlook.com>
2022-03-13 18:18:15 +0530

    Adding dependency on DataPower operator module
    Signed-off-by: Cloud-Native Toolkit <cloudnativetoolkit@gmail.com>

--- test/stages/stage1-datapower-operator.tf
+++ test/stages/stage1-datapower-operator.tf
@@ -1,13 +1,11 @@
-module "datapower" {
-  source = "./module"
-  depends_on = [
-    module.gitops-cp-datapower-operator
-  ]
+module "datapower-operator" {
+  source = "github.com/cloud-native-toolkit/terraform-gitops-cp-datapower-operator.git"
+
   gitops_config = module.gitops.gitops_config
   git_credentials = module.gitops.git_credentials
   server_name = module.gitops.server_name
   namespace = module.gitops_namespace.name
   catalog = module.cp_catalogs.catalog_ibmoperators
-  entitlement_key = module.cp_catalogs.entitlement_key
   kubeseal_cert = module.gitops.sealed_secrets_cert
+  entitlement_key = module.cp_catalogs.entitlement_key
 }
