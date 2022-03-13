920ade69e7c1eb13a4d33023e1eb578a018b5506
kanand3 <keshav.anand@outlook.com>
2022-03-13 17:28:11 +0530

    Removing DataPower operator components from this module.
    Signed-off-by: Cloud-Native Toolkit <cloudnativetoolkit@gmail.com>

--- .github/scripts/validate-deploy.sh
+++ .github/scripts/validate-deploy.sh
@@ -13,79 +13,6 @@ cd .testrepo || exit 1
 
 find . -name "*"
 
-NAMESPACE="openshift-operators"
-BRANCH="main"
-SERVER_NAME="default"
-TYPE="operators"
-
-echo "<-------------Printing contents of gitops-output.json------------->"
-cat gitops-output.json
-
-COMPONENT_NAME="ibm-datapower"
-
-if [[ ! -f "argocd/2-services/cluster/${SERVER_NAME}/${TYPE}/${NAMESPACE}-${COMPONENT_NAME}.yaml" ]]; then
-  echo "ArgoCD config missing - argocd/2-services/cluster/${SERVER_NAME}/${TYPE}/${NAMESPACE}-${COMPONENT_NAME}.yaml"
-  exit 1
-fi
-
-echo "Printing argocd/2-services/cluster/${SERVER_NAME}/${TYPE}/${NAMESPACE}-${COMPONENT_NAME}.yaml"
-cat "argocd/2-services/cluster/${SERVER_NAME}/${TYPE}/${NAMESPACE}-${COMPONENT_NAME}.yaml"
-
-if [[ ! -f "payload/2-services/namespace/${NAMESPACE}/${COMPONENT_NAME}/values.yaml" ]]; then
-  echo "Application values not found - payload/2-services/namespace/${NAMESPACE}/${COMPONENT_NAME}/values.yaml"
-  exit 1
-fi
-
-echo "Printing payload/2-services/namespace/${NAMESPACE}/${COMPONENT_NAME}/values.yaml"
-cat "payload/2-services/namespace/${NAMESPACE}/${COMPONENT_NAME}/values.yaml"
-
-
-count=0
-until kubectl get namespace "${NAMESPACE}" 1> /dev/null 2> /dev/null || [[ $count -eq 20 ]]; do
-  echo "Waiting for namespace: ${NAMESPACE}"
-  count=$((count + 1))
-  sleep 15
-done
-
-if [[ $count -eq 20 ]]; then
-  echo "Timed out waiting for namespace: ${NAMESPACE}"
-  exit 1
-else
-  echo "Found namespace: ${NAMESPACE}. Sleeping for 30 seconds to wait for everything to settle down"
-  sleep 30
-fi
-
-SUBSCRIPTION="subscription/datapower-operator"
-count=0
-until kubectl get ${SUBSCRIPTION} -n "${NAMESPACE}"  || [[ $count -eq 20 ]]; do
-  echo "Waiting for ${SUBSCRIPTION} in ${NAMESPACE}"
-  count=$((count + 1))
-  sleep 15
-done
-
-if [[ $count -eq 20 ]]; then
-  echo "Timed out waiting for ${SUBSCRIPTION} in ${NAMESPACE}"
-  kubectl get subscription -n "${NAMESPACE}"
-  exit 1
-fi
-
-CSV="datapower-operator"
-count=0
-until [[ $(kubectl get csv -n "${NAMESPACE}" -l operators.coreos.com/${CSV}.${NAMESPACE}="" -o=jsonpath='{range .items[]}{.metadata.name}{"\n"}{end}' | wc -l) -gt 0 ]] || [[ $count -eq 20 ]]; do
-  echo "Waiting for csv ${CSV} in ${NAMESPACE}"
-  count=$((count + 1))
-  sleep 15
-done
-
-if [[ $count -eq 20 ]]; then
-  echo "Timed out waiting for ${CSV} in ${NAMESPACE}"
-  kubectl get csv -n "${NAMESPACE}"
-  exit 1
-fi
-
-echo "Found CSV"
-kubectl get csv -n "${NAMESPACE}" -l operators.coreos.com/${CSV}.${NAMESPACE}="" -o=jsonpath='{range .items[]}{.metadata.name}{"\n"}{end}'
-
 ## ***** Instance
 
 NAMESPACE="gitops-cp-datapower"
@@ -103,13 +30,13 @@ fi
 echo "Printing argocd/2-services/cluster/${SERVER_NAME}/${TYPE}/${NAMESPACE}-${COMPONENT_NAME}.yaml"
 cat "argocd/2-services/cluster/${SERVER_NAME}/${TYPE}/${NAMESPACE}-${COMPONENT_NAME}.yaml"
 
-if [[ ! -f "payload/2-services/namespace/${NAMESPACE}/${COMPONENT_NAME}/values.yaml" ]]; then
-  echo "Application values not found - payload/2-services/namespace/${NAMESPACE}/${COMPONENT_NAME}/values.${SERVER_NAME}.yaml"
+if [[ ! -f "payload/2-services/namespace/${NAMESPACE}/${COMPONENT_NAME}/values-${SERVER_NAME}.yaml" ]]; then
+  echo "Application values not found - payload/2-services/namespace/${NAMESPACE}/${COMPONENT_NAME}/values-${SERVER_NAME}.yaml"
   exit 1
 fi
 
-echo "Printing payload/2-services/namespace/${NAMESPACE}/${COMPONENT_NAME}/values.${SERVER_NAME}.yaml"
-cat "payload/2-services/namespace/${NAMESPACE}/${COMPONENT_NAME}/values.${SERVER_NAME}.yaml"
+echo "Printing payload/2-services/namespace/${NAMESPACE}/${COMPONENT_NAME}/values-${SERVER_NAME}.yaml"
+cat "payload/2-services/namespace/${NAMESPACE}/${COMPONENT_NAME}/values-${SERVER_NAME}.yaml"
 
 cd ..
 rm -rf .testrepo
@@ -117,10 +44,10 @@ rm -rf .testrepo
 INSTANCE_NAME="dp"
 CR="datapowerservice/${INSTANCE_NAME}"
 count=0
-until kubectl get "${CR}" -n "${NAMESPACE}" || [[ $count -eq 20 ]]; do
+until kubectl get "${CR}" -n "${NAMESPACE}" || [[ $count -eq 10 ]]; do
   echo "Waiting for ${CR} in ${NAMESPACE}"
   count=$((count + 1))
-  sleep 15
+  sleep 60
 done
 
 if [[ $count -eq 20 ]]; then
--- main.tf
+++ main.tf
@@ -3,24 +3,10 @@ locals {
   subscription_name = local.base_name
   instance_name = "${local.base_name}-instance"
   bin_dir       = module.setup_clis.bin_dir
-  subscription_chart_dir = "${path.module}/charts/${local.base_name}"
-  subscription_yaml_dir = "${path.cwd}/.tmp/${local.base_name}/chart/${local.subscription_name}"
+
   instance_chart_dir = "${path.module}/charts/${local.instance_name}"
   instance_yaml_dir     = "${path.cwd}/.tmp/${local.base_name}/chart/${local.instance_name}"
-  subscription_values_content = {
-      subscriptions = {
-        ibmdatapower = {
-          name         = "datapower-operator"
-          subscription = {
-            channel             = var.channel
-            installPlanApproval = "Automatic"
-            name                = "datapower-operator"
-            source              = var.catalog
-            sourceNamespace     = var.catalog_namespace
-          }
-        }
-      }
-  }
+
 
   instance_values_content = {
     datapower_instance = {
@@ -72,50 +58,6 @@ module setup_clis {
   source = "github.com/cloud-native-toolkit/terraform-util-clis.git"
 }
 
-resource null_resource create_subscription_yaml {
-  provisioner "local-exec" {
-    command = "${path.module}/scripts/create-yaml.sh '${local.subscription_name}' '${local.subscription_chart_dir}' '${local.subscription_yaml_dir}' '${local.values_file}'"
-
-    environment = {
-      VALUES_CONTENT = yamlencode(local.subscription_values_content)
-    }
-  }
-}
-
-resource null_resource setup_subscription_gitops {
-  depends_on = [null_resource.create_subscription_yaml]
-
-  triggers = {
-    bin_dir = local.bin_dir
-    name = local.subscription_name
-    namespace = var.subscription_namespace
-    yaml_dir = local.subscription_yaml_dir
-    server_name = var.server_name
-    layer = local.layer
-    type = "operators"
-    git_credentials = yamlencode(var.git_credentials)
-    gitops_config   = yamlencode(var.gitops_config)
-  }
-
-  provisioner "local-exec" {
-    command = "${self.triggers.bin_dir}/igc gitops-module '${self.triggers.name}' -n '${self.triggers.namespace}' --contentDir '${self.triggers.yaml_dir}' --serverName '${self.triggers.server_name}' -l '${self.triggers.layer}' --type='${self.triggers.type}' --valueFiles='values.yaml,${local.values_file}'"
-
-    environment = {
-      GIT_CREDENTIALS = nonsensitive(self.triggers.git_credentials)
-      GITOPS_CONFIG   = self.triggers.gitops_config
-    }
-  }
-
-  provisioner "local-exec" {
-    when = destroy
-    command = "${self.triggers.bin_dir}/igc gitops-module '${self.triggers.name}' -n '${self.triggers.namespace}' --delete --contentDir '${self.triggers.yaml_dir}' --serverName '${self.triggers.server_name}' -l '${self.triggers.layer}' --type '${self.triggers.type}'"
-
-    environment = {
-      GIT_CREDENTIALS = nonsensitive(self.triggers.git_credentials)
-      GITOPS_CONFIG   = self.triggers.gitops_config
-    }
-  }
-}
 
 module pull_secret {
   source = "github.com/cloud-native-toolkit/terraform-gitops-pull-secret"
