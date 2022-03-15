fa5fdec8c14759bf2f3c2c1ed1ef9da721ffcb2e
kanand3 <keshav.anand@outlook.com>
2022-03-15 17:08:48 +0530

    Pulling variables from CP4I dependencies.
    Signed-off-by: Cloud-Native Toolkit <cloudnativetoolkit@gmail.com>

--- test/stages/stage2-datapower.tf
+++ test/stages/stage2-datapower.tf
@@ -1,8 +1,8 @@
 module "datapower" {
   source = "./module"
-  #depends_on = [
-  #  module.gitops_cp_datapower_operator
-  #]
+  depends_on = [
+    module.datapower-operator
+  ]
   gitops_config = module.gitops.gitops_config
   git_credentials = module.gitops.git_credentials
   server_name = module.gitops.server_name
@@ -10,4 +10,10 @@ module "datapower" {
   catalog = module.cp_catalogs.catalog_ibmoperators
   entitlement_key = module.cp_catalogs.entitlement_key
   kubeseal_cert = module.gitops.sealed_secrets_cert
+
+  # Pulling variables from CP4I dependency management
+  dpReleaseVersion  = module.cp4i-dependencies.datapower.version
+  dpLicense    = module.cp4i-dependencies.datapower.license
+  dpLicenseUse = module.cp4i-dependencies.datapower.license_use
+
 }
