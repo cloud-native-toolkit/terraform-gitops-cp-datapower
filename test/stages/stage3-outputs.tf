f27d7f941927a6f81c2157f192408354d2506756
gowdhaman-j <gowdhaman_j@yahoo.com>
2022-06-28 16:08:30 +0530

    Updated the module
    Signed-off-by: gowdhaman-j <gowdhaman_j@yahoo.com>

--- test/stages/stage3-outputs.tf
+++ test/stages/stage3-outputs.tf
@@ -10,7 +10,7 @@ resource null_resource write_outputs {
         namespace   = module.gitops.namespace
         server_name = module.gitops.server_name
         layer       = module.gitops.layer
-        layer_dir   = module.gitops.layer == "infrastructure" ? "1-infrastructure" : (module.gitops_module.layer == "services" ? "2-services" : "3-applications")
+        layer_dir   = module.gitops.layer == "infrastructure" ? "1-infrastructure" : (module.gitops.layer == "services" ? "2-services" : "3-applications")
         type        = module.gitops.type
       })
     }
