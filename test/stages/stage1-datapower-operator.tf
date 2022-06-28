66c145e971a856b49ac8304a501485d109ba3065
gowdhaman-j <gowdhaman_j@yahoo.com>
2022-06-28 15:55:35 +0530

    Updated the module
    Signed-off-by: gowdhaman-j <gowdhaman_j@yahoo.com>

--- test/stages/stage1-datapower-operator.tf
+++ test/stages/stage1-datapower-operator.tf
@@ -5,5 +5,7 @@ module "datapower-operator" {
   git_credentials = module.gitops.git_credentials
   server_name = module.gitops.server_name
   catalog = module.cp_catalogs.catalog_ibmoperators
+  namespace       = module.gitops_namespace.name
+  entitlement_key = module.cp_catalogs.entitlement_key
   channel = module.cp4i-dependencies.datapower.channel
 }
