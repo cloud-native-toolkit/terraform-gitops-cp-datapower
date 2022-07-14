44579f7600b1d518d81de329f74d9182d8f97bea
gowdhaman-j <gowdhaman_j@yahoo.com>
2022-07-14 16:06:37 +0530

    Updated for CP4i 2022.2.1
    Signed-off-by: gowdhaman-j <gowdhaman_j@yahoo.com>

--- test/stages/stage1-datapower-operator.tf
+++ test/stages/stage1-datapower-operator.tf
@@ -4,6 +4,7 @@ module "datapower-operator" {
   gitops_config = module.gitops.gitops_config
   git_credentials = module.gitops.git_credentials
   server_name = module.gitops.server_name
-  namespace = module.gitops_namespace.name
+  catalog = module.cp_catalogs.catalog_ibmoperators
+  channel = module.cp4i-dependencies.mq.channel
 
 }
