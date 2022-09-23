e35e8f8e1befe7dd4aafd62d84c0cf5c0578f7eb
balasgit <bala@us.ibm.com>
2022-09-23 17:49:45 -0500

    updated gitops provider
    Signed-off-by: balasgit <bala@us.ibm.com>

--- main.tf
+++ main.tf
@@ -90,37 +90,52 @@ resource null_resource create_instance_yaml {
   }
 }
 
-resource null_resource setup_instance_gitops {
+resource gitops_module module {
   depends_on = [null_resource.create_instance_yaml]
 
-  triggers = {
-    bin_dir = local.bin_dir
-    name = local.instance_name
-    namespace = var.namespace
-    yaml_dir = local.instance_yaml_dir
-    server_name = var.server_name
-    layer = local.layer
-    type = local.type
-    git_credentials = yamlencode(var.git_credentials)
-    gitops_config   = yamlencode(var.gitops_config)
-  }
 
-  provisioner "local-exec" {
-    command = "${self.triggers.bin_dir}/igc gitops-module '${self.triggers.name}' -n '${self.triggers.namespace}' --contentDir '${self.triggers.yaml_dir}' --serverName '${self.triggers.server_name}' -l '${self.triggers.layer}' --type=${self.triggers.type} --valueFiles='${local.values_file}'"
-
-    environment = {
-      GIT_CREDENTIALS = nonsensitive(self.triggers.git_credentials)
-      GITOPS_CONFIG   = self.triggers.gitops_config
-    }
-  }
-
-  provisioner "local-exec" {
-    when = destroy
-    command = "${self.triggers.bin_dir}/igc gitops-module '${self.triggers.name}' -n '${self.triggers.namespace}' --delete --contentDir '${self.triggers.yaml_dir}' --serverName '${self.triggers.server_name}' -l '${self.triggers.layer}' --type '${self.triggers.type}'"
-
-    environment = {
-      GIT_CREDENTIALS = nonsensitive(self.triggers.git_credentials)
-      GITOPS_CONFIG   = self.triggers.gitops_config
-    }
-  }
+  name = local.base_name
+  #namespace = local.namespace
+  content_dir = local.instance_yaml_dir
+  server_name = var.server_name
+  layer = local.layer
+  type = local.type
+  branch = local.application_branch
+  config = yamlencode(var.gitops_config)
+  credentials = yamlencode(var.git_credentials)
 }
+
+# resource null_resource setup_instance_gitops {
+#   depends_on = [null_resource.create_instance_yaml]
+
+#   triggers = {
+#     bin_dir = local.bin_dir
+#     name = local.instance_name
+#     namespace = var.namespace
+#     yaml_dir = local.instance_yaml_dir
+#     server_name = var.server_name
+#     layer = local.layer
+#     type = local.type
+#     git_credentials = yamlencode(var.git_credentials)
+#     gitops_config   = yamlencode(var.gitops_config)
+#   }
+
+#   provisioner "local-exec" {
+#     command = "${self.triggers.bin_dir}/igc gitops-module '${self.triggers.name}' -n '${self.triggers.namespace}' --contentDir '${self.triggers.yaml_dir}' --serverName '${self.triggers.server_name}' -l '${self.triggers.layer}' --type=${self.triggers.type} --valueFiles='${local.values_file}'"
+
+#     environment = {
+#       GIT_CREDENTIALS = nonsensitive(self.triggers.git_credentials)
+#       GITOPS_CONFIG   = self.triggers.gitops_config
+#     }
+#   }
+
+#   provisioner "local-exec" {
+#     when = destroy
+#     command = "${self.triggers.bin_dir}/igc gitops-module '${self.triggers.name}' -n '${self.triggers.namespace}' --delete --contentDir '${self.triggers.yaml_dir}' --serverName '${self.triggers.server_name}' -l '${self.triggers.layer}' --type '${self.triggers.type}'"
+
+#     environment = {
+#       GIT_CREDENTIALS = nonsensitive(self.triggers.git_credentials)
+#       GITOPS_CONFIG   = self.triggers.gitops_config
+#     }
+#   }
+# }
