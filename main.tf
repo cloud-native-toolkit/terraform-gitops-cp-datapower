fbbb75402cf659e78366816eb3155802b281c8a4
Bala Sivasubramanian <bala@us.ibm.com>
2022-09-30 13:57:33 -0500

    Updates gitops provider (#17)
    support gitops provider and gitea
    
    Signed-off-by: balasgit <bala@us.ibm.com>
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>
    Co-authored-by: Sean Sundberg <seansund@us.ibm.com>
    Co-authored-by: Andrew Trice <amtrice@us.ibm.com>

--- main.tf
+++ main.tf
@@ -2,7 +2,6 @@ locals {
   base_name     = "ibm-datapower"
   subscription_name = local.base_name
   instance_name = "${local.base_name}-instance"
-  bin_dir       = module.setup_clis.bin_dir
 
   instance_chart_dir = "${path.module}/charts/${local.instance_name}"
   instance_yaml_dir     = "${path.cwd}/.tmp/${local.base_name}/chart/${local.instance_name}"
@@ -61,23 +60,21 @@ locals {
   layer_config = var.gitops_config[local.layer]
 }
 
-module setup_clis {
-  source = "github.com/cloud-native-toolkit/terraform-util-clis.git"
-}
 
-
-module pull_secret {
-  source = "github.com/cloud-native-toolkit/terraform-gitops-pull-secret"
-
-  gitops_config = var.gitops_config
-  git_credentials = var.git_credentials
-  server_name = var.server_name
-  kubeseal_cert = var.kubeseal_cert
+resource gitops_pull_secret cp_icr_io {
+  name = "ibm-entitlement-key"
   namespace = var.namespace
-  docker_username = "cp"
-  docker_password = var.entitlement_key
-  docker_server   = "cp.icr.io"
-  secret_name     = "ibm-entitlement-key"
+  server_name = var.server_name
+  branch = local.application_branch
+  layer = local.layer
+  credentials = yamlencode(var.git_credentials)
+  config = yamlencode(var.gitops_config)
+  kubeseal_cert = var.kubeseal_cert
+
+  secret_name = "ibm-entitlement-key"
+  registry_server = "cp.icr.io"
+  registry_username = "cp"
+  registry_password = var.entitlement_key
 }
 
 resource null_resource create_instance_yaml {
@@ -90,37 +87,17 @@ resource null_resource create_instance_yaml {
   }
 }
 
-resource null_resource setup_instance_gitops {
+resource gitops_module setup_gitops {
   depends_on = [null_resource.create_instance_yaml]
 
-  triggers = {
-    bin_dir = local.bin_dir
-    name = local.instance_name
-    namespace = var.namespace
-    yaml_dir = local.instance_yaml_dir
-    server_name = var.server_name
-    layer = local.layer
-    type = local.type
-    git_credentials = yamlencode(var.git_credentials)
-    gitops_config   = yamlencode(var.gitops_config)
-  }
-
-  provisioner "local-exec" {
-    command = "${self.triggers.bin_dir}/igc gitops-module '${self.triggers.name}' -n '${self.triggers.namespace}' --contentDir '${self.triggers.yaml_dir}' --serverName '${self.triggers.server_name}' -l '${self.triggers.layer}' --type=${self.triggers.type} --valueFiles='${local.values_file}'"
-
-    environment = {
-      GIT_CREDENTIALS = nonsensitive(self.triggers.git_credentials)
-      GITOPS_CONFIG   = self.triggers.gitops_config
-    }
-  }
-
-  provisioner "local-exec" {
-    when = destroy
-    command = "${self.triggers.bin_dir}/igc gitops-module '${self.triggers.name}' -n '${self.triggers.namespace}' --delete --contentDir '${self.triggers.yaml_dir}' --serverName '${self.triggers.server_name}' -l '${self.triggers.layer}' --type '${self.triggers.type}'"
-
-    environment = {
-      GIT_CREDENTIALS = nonsensitive(self.triggers.git_credentials)
-      GITOPS_CONFIG   = self.triggers.gitops_config
-    }
-  }
+  name = local.instance_name
+  namespace = var.namespace
+  content_dir = local.instance_yaml_dir
+  server_name = var.server_name
+  layer = local.layer
+  type = local.type
+  branch = local.application_branch
+  config = yamlencode(var.gitops_config)
+  credentials = yamlencode(var.git_credentials)
 }
+
